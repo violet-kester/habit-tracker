6b388bb33bb5ec08a961cdbdd2cda3a60edb84d7
Violet Kester <kestervj89@gmail.com>
2024-01-31 21:08:15 -0500

    registered Habit and Progress models to admin page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Habit, Progress
 
-# Register your models here.
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    # Displayed columns
+    list_display = ['id', 'user', 'name', 'slug', 'description']
+    # Column filters
+    list_filter = ['user']
+    # Editable fields
+    fields = ['id', 'user', 'name', 'slug', 'description']
+    # Prepopulate slug field based on name
+    prepopulated_fields = {'slug': ('name',)}
+    # Order habits by user then name by default
+    ordering = ['user', 'name']
+
+
+@admin.register(Progress)
+class ProgressAdmin(admin.ModelAdmin):
+    # Displayed columns
+    list_display = ['id', 'habit', 'date', 'completed']
+    # Column filters
+    list_filter = ['habit', 'date', 'completed']
