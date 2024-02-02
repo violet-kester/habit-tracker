7282bb7c914d4b0de7482cb8d6db754253486f5f
Violet Kester <kestervj89@gmail.com>
2024-02-02 12:16:09 -0500

    added url patterns around auth

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,8 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
